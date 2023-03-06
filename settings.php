ef283dd3be0319f833fd6d249245b9836ea1cf98
зеркен <107109714+therken@users.noreply.github.com>
2023-03-06 13:43:54 +0300

    in

--- settings.php
+++ settings.php
@@ -3,4 +3,35 @@ $host= 'localhost';
 $user = 'root';
 $pass = '';
 $db = 'forma';
+  
+$conn = mysqli_connect($host , $user , $pass , $db);
+// Check connection
+if($conn === false){
+  die("ERROR: Could not connect. "
+      . mysqli_connect_error());
+}
+
+$name = mysqli_real_escape_string($conn ,$_POST['name']);
+$sec = mysqli_real_escape_string($conn,$_POST['secondname']);
+$last = mysqli_real_escape_string($conn,$_POST['lastname']);
+$mail = mysqli_real_escape_string($conn,$_POST['mail']);
+$about = mysqli_real_escape_string($conn,$_POST['about']);
+$edate=strtotime($_POST['date']); 
+$edate=date("Y-m-d",$edate);
+
+$folder ='uploads/';
+$file_ext =  strtolower(strrchr($_FILES['image']['name'],'.'));
+$file_name = "file".uniqid(rand(10000,99999));
+$uploadedFile  = $folder.$file_name.$file_ext;
+$sql = "INSERT INTO user_form (firstname, secondname, lastname, mail, about, dendata,img)
+VALUES ('$name', '$sec', '$last', '$mail', '$about', '$edate','$file_name')";
+
+if(mysqli_query($conn, $sql)){
+  echo "<h3>data stored in a database successfully.</h3>";  
+} else{
+  echo "ERROR: Hush! Sorry $sql. "
+      . mysqli_error($conn);
+}
+
+mysqli_close($conn);
 ?>
