8c2e116be09b27bbca6907d90a5141516999e6a2
kaguhe <107109714+therken@users.noreply.github.com>
2023-03-09 16:21:01 +0600

    Update settings.php

--- settings.php
+++ settings.php
@@ -19,9 +19,10 @@ $about = mysqli_real_escape_string($conn,$_POST['about']);
 $edate=strtotime($_POST['date']); 
 $edate=date("Y-m-d",$edate);
 
-$folder ='uploads/';
+$folder ="./uploads/";
 $file_ext =  strtolower(strrchr($_FILES['image']['name'],'.'));
 $file_name = "file".uniqid(rand(10000,99999));
+move_uploaded_file($tmp_name, "$uploads_dir/$name")
 $uploadedFile  = $folder.$file_name.$file_ext;
 $sql = "INSERT INTO user_form (firstname, secondname, lastname, mail, about, dendata,img)
 VALUES ('$name', '$sec', '$last', '$mail', '$about', '$edate','$file_name')";
