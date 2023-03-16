e6b3a6abe5bbe257a689f5b8280ae9c02b535507
kaguhe <107109714+therken@users.noreply.github.com>
2023-03-16 07:09:42 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,10 +7,11 @@
 <title>trytodie</title>
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"><link rel="stylesheet" href="style.css">
 <link href='bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css' rel='stylesheet' type='text/css'>
-<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js'></script><script  src="./script.js"></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js'></script><script  src="./script.js"></script> 
+
 </head>
 <body>
-<form action="profile.php" method="POST" enctype="multipart/form-data" >
+<form action="profile.php" method="POST" enctype="multipart/form-data"  >
 <div class="wrapper">
 <!--image-->
 <div class="avatar-wrapper">
@@ -20,19 +21,20 @@
 <input class="file-upload" type="file" accept="image/*" name="image"/>
 </div>
 <!--1-->
-<input type="text" class="form-control input-xs" name="name" placeholder="Имя">
+<input type="text" class="form-control input-xs" name="name" placeholder="Имя" required>
 <!--2-->
-<input type="text" class="form-control input-xs"  name="secondname" placeholder="Фамилия">
+<input type="text" class="form-control input-xs"  name="secondname" placeholder="Фамилия" required>
 <!--3-->
-<input type="text" class="form-control input-xs"  name="lastname" placeholder="Отчество">
+<input type="text" class="form-control input-xs"  name="lastname" placeholder="Отчество" required>
 <!--4-->
 <!--5--> 
 <input type="email" name="mail" placeholder="Введите почту">
 <!--6-->
-<textarea maxlength = "1000" name="about" placeholder="о себе"></textarea>
-<input type='date' name="date" class="form-control" data-provide="datepicker" placeholder='Select Date' style='width: 300px;' > <br>
+<textarea maxlength = "1000" name="about" placeholder="о себе" required></textarea>
+<input type='date' name="date" class="form-control" data-provide="datepicker" placeholder='Select Date' style='width: 300px;' required> <br>
 <input type="submit" placeholder="Отправить" value="Сохранить">
 </div> 
 </form>
+
 </body>
 </html>
